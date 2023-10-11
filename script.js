27f22a7a402bc3f78cc34b38468f953284e92a80
LeSserafimCodeG0D <145377859+BladeeZX@users.noreply.github.com>
2023-10-11 20:19:38 +0200

    Added function round. Need to add content to function Round.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
-
-
-
+let playerChoice = prompt("What is your choice?");
 
 function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"];
     let randomElement = choice[Math.floor(Math.random() * choice.length)];
     return randomElement;
+}
+
+function round() {
+    let computerSelection = getComputerChoice();
+    let playerSelection = playerChoice;
 }

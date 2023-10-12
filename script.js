25a3a1b68e07be86b3f2d5deba9f2179957ee1e8
LeSserafimCodeG0D <145377859+BladeeZX@users.noreply.github.com>
2023-10-12 18:43:35 +0200

    Added function Round, Added comments, defined user input.

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
+alert('Welcome to the hunger games');
+let paper = "paper";
+let rock = "rock";
+let scissors = "scissors";
 
-let playerChoice = prompt("What is your choice?");
+
+let userInput = prompt("Choose Wisely");
+
+/*defines what happens with input and converts input to lover case*/
+
+let playerChoice = userInput => {
+
+    userInput = userInput.toLowerCase();
+    if (userInput === "rock"){
+      return userInput;
+    } else if (userInput === "paper"){
+      return userInput;
+    } else if (userInput === "scissors"){
+      return userInput;
+    } else{
+      console.log("Error! not valid input.")
+      return;
+    
+    }
+
+  }
+
+/* Allows computer to select random thing from array*/
 
 function getComputerChoice() {
-    let choice = ["Rock", "Paper", "Scissors"];
+
+    let choice = [rock, paper, scissors];
     let randomElement = choice[Math.floor(Math.random() * choice.length)];
     return randomElement;
+
 }
 
+let computerChoice = getComputerChoice();
+
+/* Allows round to play out */
 function round() {
-    let computerSelection = getComputerChoice();
-    let playerSelection = playerChoice;
+
+    if (computerChoice === playerChoice) {
+        result = "Draw";
+    } else if (
+        (playerChoice === rock && computerChoice === paper) ||
+        (playerChoice === paper && computerChoice === scissors) ||
+        (playerChoice === scissors && computerChoice === rock)
+    ) {
+        result = "You lose this round";
+    }
+    else {
+        result = "You win this round";
+    }
+
 }
+
+/*let playerChoice = prompt("What is your choice?");*/
+round();
+
+console.log(round(playerChoice, computerChoice));

9e12654582f7d959a80ac0c1e82392177592eadb
LeSserafimCodeG0D <145377859+BladeeZX@users.noreply.github.com>
2023-10-12 20:12:25 +0200

    Formatted code to look better, added function game

--- script.js
+++ script.js
@@ -1,59 +1,54 @@
-alert('Welcome to the hunger games');
+alert("Welcome to the hunger games");
 let userInput = prompt("Choose Wisely");
 
 /*defines what happens with input and converts input to lover case*/
 
 function getPlayerChoice() {
-    /*Turns input to lower case for the sake of clarity and fixing errors*/
-    userInput = userInput.toLowerCase();
+  /*Turns input to lower case for the sake of clarity and fixing errors*/
+  userInput = userInput.toLowerCase();
 
-    if (userInput === "rock") {
-      return userInput;
-    } else if (userInput === "paper"){
-      return userInput;
-    } else if (userInput === "scissors"){
-      return userInput;
-    } else {
-      console.log("Error! not valid input. Enter Valid Input")
-      return getPlayerChoice();
-    }
-
+  if (userInput === "rock") {
+    return userInput;
+  } else if (userInput === "paper") {
+    return userInput;
+  } else if (userInput === "scissors") {
+    return userInput;
+  } else {
+    console.log("Error! not valid input. Enter Valid Input");
+    return getPlayerChoice();
   }
+}
 
 /* Allows computer to select random thing from array*/
 
 function getComputerChoice() {
-
-    let choice = ["rock", "paper", "scissors"];
-    let randomElement = choice[Math.floor(Math.random() * choice.length)];
-    return randomElement;
-
+  let choice = ["rock", "paper", "scissors"];
+  let randomElement = choice[Math.floor(Math.random() * choice.length)];
+  return randomElement;
 }
 
 let computerChoice = getComputerChoice();
 /*it works till this moment*/
 
-
-/* Allows round to play out */
-function round() {
+function game() {
+  /* Allows round to play out */
+  function round() {
     let result;
     let playerChoice = getPlayerChoice();
     if (computerChoice === playerChoice) {
-        result = "Draw";
+      result = "Draw";
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        result = "You lose, paper beats rock!";
+      result = "You lose, paper beats rock!";
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        result = "You lose, scissors cut paper!";
+      result = "You lose, scissors cut paper!";
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        result = "You lose, scissors shatter on a rock!";
+      result = "You lose, scissors shatter on a rock!";
     } else {
-        result = "You win this round";
+      result = "You win this round";
     }
     return result;
+  }
 }
-
-
-
 let playerSelection = getPlayerChoice();
 let computerSelection = getComputerChoice();
 let result = round(playerSelection, computerSelection);
